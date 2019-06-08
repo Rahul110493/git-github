1d484c80443b0dc30e4811274c3b75738d5903bf
rahul_abrol <rahul.abrol@successive.tech>
2019-06-08 11:35:01 +0530

    Issue 2

--- brigade.js
+++ brigade.js
@@ -12,12 +12,11 @@ events.on("push", function(e, project) {
 
   // Now we want it to run these commands in order:
   node.tasks = [
-    
+ 
     "cd /src/app",
     "pip install -r requirements.txt",
     "cd /src/",
-    "python setup.py test",
-    "pwd",
+    "python setup.py test"
   ]
 
   // We're done configuring, so we run the job
